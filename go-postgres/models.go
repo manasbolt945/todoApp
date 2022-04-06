6245ac153ff091efbbeffd0554af0dc6a02807f9
Manas Pandey <30745064+manasbolt945@users.noreply.github.com>
2022-04-06 15:19:20 +0530

    Add files via upload

--- go-postgres/models.go
+++ go-postgres/models.go
@@ -1,2 +1,15 @@
 package models
 
+import(
+	"time"
+	"github.com/google/uuid"
+)
+
+type Todo struct{
+	ID uuid.UUID 'gorm:"type:uuid;default:uuid_generate_v4();primaryKey;"'
+	Desc string
+	Status string
+	Created_At  time.Time 'gorm:"autoCreateTime"'
+	Updated_At time.Time  'gorm:"autoUpdateTime'
+}
+
